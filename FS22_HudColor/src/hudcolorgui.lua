087649d161c592adf5db2d41eea34d75cf54350a
Dennis Schmitt <peppie23@gmail.com>
2024-06-24 21:37:29 +0200

    initial

---
+++ FS22_HudColor/src/hudcolorgui.lua
@@ -0,0 +1,144 @@
+---
+-- ColorHudGui
+--
+-- Class to handle the new ui controls on the settings frame
+-- to control the color for the hud and saves the
+-- value in the modSettings-folder.
+--
+-- Copyright (c) Peppie84, 2024
+--
+HudColorGui = {
+    MOD_DIRECTORY = g_currentModDirectory,
+    MOD_SETTINGS_DIRECTORY = g_currentModSettingsDirectory .. '../',
+    MOD_SETTINGS_FILENAME = 'hudcolor.xml',
+    MOD_SETTINGS_XML_ROOT_NODE = 'settings',
+    CURRENT_MOD = g_currentModName or 'unknown',
+    L10N_SYMBOLS = {
+        MOD_TITLE = 'mod_title',
+        HUD_COLOR_SETTING_LABEL = 'settings_hud_color_label',
+        HUD_COLOR_SETTING_RESTART = 'settings_hud_color_label_needs_restart',
+        HUD_COLOR_SETTING_DESCRIPTION = 'settings_hud_color_description',
+        EASYARMCONTROL_COLOR_OPTION1 = 'settings_hud_color_option1',
+        EASYARMCONTROL_COLOR_OPTION2 = 'settings_hud_color_option2',
+        EASYARMCONTROL_COLOR_OPTION3 = 'settings_hud_color_option3',
+        EASYARMCONTROL_COLOR_OPTION4 = 'settings_hud_color_option4',
+        EASYARMCONTROL_COLOR_OPTION5 = 'settings_hud_color_option5',
+        EASYARMCONTROL_COLOR_OPTION6 = 'settings_hud_color_option6',
+    },
+    ENUM_EASYARMCONTROL_INDEX = {
+        LABEL = 4,
+        DESCRIPTION = 6,
+    },
+    ENUM_COLORS = {
+        DEFAULT = 1,
+        PINK = 2,
+        ORANGE = 3,
+        RED = 4,
+        GRAY = 5,
+        GREEN = 6,
+    },
+}
+
+HudColorGui.settings = {}
+HudColorGui.settings.colorPreset = 1
+
+---Append to InGameMenuGeneralSettingsFrame.onFrameOpen
+---Initialize our gui elements for the settings frame that we need.
+function HudColorGui:initGui()
+    if not self.initHudColorGuiDone then
+        local title = TextElement.new()
+        local hudcolorSettingTitleText = g_i18n:getText(HudColorGui.L10N_SYMBOLS.MOD_TITLE, HudColorGui.CURRENT_MOD)
+        local hudColorSettingLabelText = g_i18n:getText(HudColorGui.L10N_SYMBOLS.HUD_COLOR_SETTING_LABEL, HudColorGui.CURRENT_MOD)
+        local hudColorSettingDescriptionText = g_i18n:getText(HudColorGui.L10N_SYMBOLS.HUD_COLOR_SETTING_DESCRIPTION, HudColorGui.CURRENT_MOD)
+
+        local hudColorSettingOption1Text = g_i18n:getText(HudColorGui.L10N_SYMBOLS.EASYARMCONTROL_COLOR_OPTION1, HudColorGui.CURRENT_MOD)
+        local hudColorSettingOption2Text = g_i18n:getText(HudColorGui.L10N_SYMBOLS.EASYARMCONTROL_COLOR_OPTION2, HudColorGui.CURRENT_MOD)
+        local hudColorSettingOption3Text = g_i18n:getText(HudColorGui.L10N_SYMBOLS.EASYARMCONTROL_COLOR_OPTION3, HudColorGui.CURRENT_MOD)
+        local hudColorSettingOption4Text = g_i18n:getText(HudColorGui.L10N_SYMBOLS.EASYARMCONTROL_COLOR_OPTION4, HudColorGui.CURRENT_MOD)
+        local hudColorSettingOption5Text = g_i18n:getText(HudColorGui.L10N_SYMBOLS.EASYARMCONTROL_COLOR_OPTION5, HudColorGui.CURRENT_MOD)
+        local hudColorSettingOption6Text = g_i18n:getText(HudColorGui.L10N_SYMBOLS.EASYARMCONTROL_COLOR_OPTION6, HudColorGui.CURRENT_MOD)
+
+        self.HudColor = self.checkUseEasyArmControl:clone()
+        self.HudColor.target = HudColorGui
+        self.HudColor.id = 'HudColor'
+        self.HudColor:setCallback('onClickCallback', 'onHudColorChanged')
+        self.HudColor:setTexts({hudColorSettingOption1Text, hudColorSettingOption2Text, hudColorSettingOption3Text, hudColorSettingOption4Text, hudColorSettingOption5Text, hudColorSettingOption6Text})
+        self.HudColor.elements[HudColorGui.ENUM_EASYARMCONTROL_INDEX.LABEL]:setText(hudColorSettingLabelText)
+        self.HudColor.elements[HudColorGui.ENUM_EASYARMCONTROL_INDEX.DESCRIPTION]:setText(hudColorSettingDescriptionText)
+
+        g_hudColor.HudColorElement = self.HudColor
+
+        title:applyProfile('settingsMenuSubtitle', true)
+        title:setText(hudcolorSettingTitleText)
+
+        self.boxLayout:addElement(title)
+        self.boxLayout:addElement(self.HudColor)
+
+        self.initHudColorGuiDone = true
+    end
+
+    self.HudColor:setState(HudColorGui.settings.colorPreset)
+end
+
+---Callback function for our gui element by on change
+---@param state number
+function HudColorGui:onHudColorChanged(state)
+    local hudColorSettingLabelText = g_i18n:getText(HudColorGui.L10N_SYMBOLS.HUD_COLOR_SETTING_LABEL, HudColorGui.CURRENT_MOD)
+    local hudColorSettingLabelTextRestart = g_i18n:getText(HudColorGui.L10N_SYMBOLS.HUD_COLOR_SETTING_RESTART, HudColorGui.CURRENT_MOD)
+    HudColorGui.settings.colorPreset = state
+
+    if HudColorGui.settings.colorPreset ~= HudColorGui.settings.loadedColorPreset then
+        g_hudColor.HudColorElement.elements[HudColorGui.ENUM_EASYARMCONTROL_INDEX.LABEL]:setText(hudColorSettingLabelText ..' '.. hudColorSettingLabelTextRestart )
+    else
+        g_hudColor.HudColorElement.elements[HudColorGui.ENUM_EASYARMCONTROL_INDEX.LABEL]:setText(hudColorSettingLabelText)
+    end
+
+	HudColorGui:saveSettings()
+end
+
+---Appand to and InGameMenuGeneralSettingsFrame.updateGameSettings()
+---Just udpate the gui
+function HudColorGui:updateGui()
+    if self.initHudColorGuiDone and self.HudColor ~= nil then
+        self.HudColor:setState(HudColorGui.settings.colorPreset)
+    end
+end
+
+---Save the settings into its own xml under modSettings/ path
+function HudColorGui:saveSettings()
+	local filename = HudColorGui.MOD_SETTINGS_DIRECTORY .. HudColorGui.MOD_SETTINGS_FILENAME
+	local xmlRootNode = HudColorGui.MOD_SETTINGS_XML_ROOT_NODE
+	local xmlFile = XMLFile.create("settingsXML", filename, xmlRootNode)
+
+	if xmlFile ~= nil then
+		xmlFile:setInt(xmlRootNode .. ".colorPreset", self.settings.colorPreset)
+
+		xmlFile:save()
+		xmlFile:delete()
+	end
+end
+
+---Load the settings xml from modSettings/
+function HudColorGui:loadSettings()
+    local filename = HudColorGui.MOD_SETTINGS_DIRECTORY .. HudColorGui.MOD_SETTINGS_FILENAME
+	local xmlRootNode = HudColorGui.MOD_SETTINGS_XML_ROOT_NODE
+	local xmlFile = XMLFile.loadIfExists("settingsXML", filename, xmlRootNode)
+
+	if xmlFile ~= nil then
+		HudColorGui.settings.colorPreset = Utils.getNoNil(xmlFile:getInt(xmlRootNode .. ".colorPreset"), HudColorGui.ENUM_COLORS.DEFAULT)
+        HudColorGui.settings.loadedColorPreset = HudColorGui.settings.colorPreset
+        g_hudColor:setPresetColor(HudColorGui.settings.colorPreset)
+        self:updateGui()
+
+		xmlFile:delete()
+	end
+end
+
+---Init
+local function init()
+    HudColorGui:loadSettings()
+    InGameMenuGeneralSettingsFrame.onFrameOpen = Utils.appendedFunction(InGameMenuGeneralSettingsFrame.onFrameOpen, HudColorGui.initGui)
+    InGameMenuGeneralSettingsFrame.updateGameSettings = Utils.appendedFunction(InGameMenuGeneralSettingsFrame.updateGameSettings, HudColorGui.updateGui)
+end
+
+init()
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# FS22_HudColor
-Change the color of the Hud. Weitere LS22 🚜 Mods: https://github.com/stars/Peppie84/lists/landwirtschafts-simulator-22
+<a name="readme-top"></a>
+
+<div align="center">
+
+[![FarmingSimulator-22](https://img.shields.io/badge/FarmingSimulator-22-blue?style=flat-square)](https://www.farming-simulator.com/)
+[![GitHub issues](https://img.shields.io/github/issues/Peppie84/FS22_HudColor?style=flat-square)](https://github.com/Peppie84/FS22_HudColor/issues)
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue?style=flat-square)](https://www.gnu.org/licenses/gpl-3.0)
+[![Last commit](https://img.shields.io/github/last-commit/Peppie84/FS22_HudColor?style=flat-square&color=important)](https://github.com/Peppie84/FS22_HudColor/commits/development)
+[![Top language](https://img.shields.io/github/languages/top/Peppie84/FS22_HudColor?style=flat-square&color=blueviolet)](https://github.com/search?q=repo%3APeppie84%2FFS22_HudColor++language%3ALua&type=code)
+
+
+<br />
+
+<img src="documents/icon_HudColor.jpg" style="width: 128px;">
+
+<h3 align="center"><u>FS22_HudColor</u></h3>
+
+<p align="center">
+    tbd
+</p>
+
+</div>
+
+<div align='center'>
+    <img src="documents/screen1-v1.0.0.0.png" style="width: 50%;">
+</div>
+
+## Credits
+* Danke an Iron_Thomas für die Idee 🙌
+
+# Copyright
+Copyright (c) 2024 [Dennis Schmitt](https://github.com/peppie84).
+All rights reserved.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
